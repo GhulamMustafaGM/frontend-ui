f0b7cd99c88215f9b96abcabea56f7f83cdc3546
Ghulam Mustafa <mustafaji@gmail.com>
2021-06-19 22:13:29 +0200

    updated App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
-import React from 'react'
-import CssBaseline   from '@material-ui/core/CssBaseline';
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import NavigationBar from './NavigationBar';
+
+import Container from '@material-ui/core/Container';
+import GridLayout from './GridLayout';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <CssBaseline />
       <NavigationBar />
+
+      <Container>
+        <main>
+          <GridLayout />
+        </main>
+      </Container>
+
     </div>
   );
 }
