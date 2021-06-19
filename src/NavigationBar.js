370d724d89cb074e06644e167c50cfe5384a7286
Ghulam Mustafa <mustafaji@gmail.com>
2021-06-19 22:59:00 +0200

    updated Frontend UI App

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -142,7 +142,7 @@ export default function NavigationBar() {
                     className={classes.siteTitle}
                 >
 
-                    Wibe App
+                    Frontend UI
 
                 </Typography>
 
