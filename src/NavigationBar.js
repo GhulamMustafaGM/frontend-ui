a7a8344d0641e5b22858e391b9e5dd3586499e91
Ghulam Mustafa <mustafaji@gmail.com>
2021-06-19 22:39:33 +0200

    updated NavigationBar

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import Container from '@material-ui/core/Container';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import Link from '@material-ui/core/Link';
+import React, { useEffect, useState } from 'react';
+
+import {
+    Container,
+    Toolbar,
+    Typography,
+    Box,
+    Link,
+    AppBar,
+    IconButton,
+    MenuItem,
+    Drawer
+} from '@material-ui/core';
+
+import MenuIcon from '@material-ui/icons/Menu';
 
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -32,28 +41,115 @@ const useStyles = makeStyles((theme) => ({
         padding: theme.spacing(1),
         [theme.breakpoints.up('md')]: {
             paddingLeft: theme.spacing(10)
-        }
+        },
+        color: 'white'
+    },
+    menuIcon: {
+        color: 'white'
     }
 
 }));
 
 export default function NavigationBar() {
 
+    const [state, setState] = useState({
+        toggleMenu: false,
+        toggleMenuOpen: false
+    });
+
+    const { toggleMenu, toggleMenuOpen } = state;
+
+    useEffect(() => {
+
+        const setResponsiveness = () => {
+
+            return window.innerWidth < 960
+                ? setState((prevState) => ({ ...prevState, toggleMenu: true }))
+                : setState((prevState) => ({ ...prevState, toggleMenu: false }));
+
+        };
+
+        setResponsiveness();
+
+        window.addEventListener("resize", () => setResponsiveness());
+
+    }, []);
+
     const classes = useStyles();
 
-    return (
-        <Container>
+    const displayToggleMenu = () => {
+
+        const handleToggleMenuOpen = () => setState((prevState) => ({ ...prevState, toggleMenuOpen: true }));
+
+        const handleToggleMenuClose = () => setState((prevState) => ({ ...prevState, toggleMenuOpen: false }));
+
+        return (
+
+            <Toolbar>
+
+                <IconButton
+                    {...{
+                        onClick: handleToggleMenuOpen
+                    }}
+                >
+                    <MenuIcon className={classes.menuIcon} />
+                </IconButton>
+
+                <Drawer
+                    {...{
+                        anchor: 'left',
+                        open: toggleMenuOpen,
+                        onClose: handleToggleMenuClose
+                    }}
+                >
+                    <div>{getToggleMenuOptions()}</div>
+                </Drawer>
+
+            </Toolbar>
+
+        );
+
+    }
+
+    const getToggleMenuOptions = () => {
+        return (
+
+            <Box>
+
+                {['home', 'courses', 'sign up'].map((menuOption) => (
+
+                    <MenuItem>
+
+                        {menuOption}
+
+                    </MenuItem>
+
+                ))}
+
+            </Box>
+        );
+    }
+
+    const displayLargeMenu = () => {
+
+        return (
+
             <Toolbar className={classes.toolbar}>
+
                 <Typography
                     component='h1'
-                    variant='h4'
+                    variant='h6'
                     className={classes.siteTitle}
                 >
-                    Wibe 
+
+                    Wibe App
+
                 </Typography>
 
                 <Box className={classes.menuBox}>
+
                     {['home', 'courses', 'sign up'].map((menuOption) => (
+
                         <Link
                             component='button'
                             variant='body1'
@@ -61,9 +157,25 @@ export default function NavigationBar() {
                         >
                             {menuOption.toUpperCase()}
                         </Link>
+
                     ))}
+
                 </Box>
+
             </Toolbar>
+
+        );
+    }
+
+    return (
+        <Container>
+
+            <AppBar>
+
+                {toggleMenu ? displayToggleMenu() : displayLargeMenu()}
+
+            </AppBar>
+
         </Container>
     );
 }
