d666708cf6f360b74c61ad1d16cf5e519c7c4288
Ghulam Mustafa <mustafaji@gmail.com>
2021-06-19 02:36:01 +0200

    created NavigationBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import CssBaseline   from '@material-ui/core/CssBaseline';
+import NavigationBar from './NavigationBar';
 
 function App() {
   return (
     <div className="App">
-
+      <CssBaseline />
+      <NavigationBar />
     </div>
   );
 }
